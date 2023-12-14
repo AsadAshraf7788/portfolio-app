8e5911361486c65493d59fe84702d4097e713d7f
Asad Ashraf <asadashraf@Asads-MacBook-Pro.local>
2023-12-15 01:53:36 +0500

    added assets, header-componebt in-progress

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ function Header() {
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border 
                   border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg
                      shadow-black/[0.03] backdrop-blur-[0.5rem] 
-                      sm:h-[3rem] sm:w-[36rem]  "
+                      sm:h-[3rem] sm:w-[36rem] sm:rounded-full "
         // initial={{ y: -100, x: "-50%", opacity: 0 }}
         // animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></div>
